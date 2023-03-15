cae1faf325cdeb8da15781251826bc4b8dc7dd8d
Sahil Kumar <sahil511kumar@gmail.com>
2023-03-16 00:55:24 +0530

    rename css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./app.css";
+import "./global.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Lyrics from "./components/Lyrics";
